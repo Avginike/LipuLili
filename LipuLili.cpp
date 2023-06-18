eb81df8ce6bcce44140e475e163f62c0ffb9e183
Avginike <janjuriewicz@gmail.com>
2023-06-18 19:26:28 +0200

    Prototype stage 1 - semi-finished encoding system.

--- LipuLili.cpp
+++ LipuLili.cpp
@@ -1,44 +1,45 @@
-
-#define WORDLIST_NAME "wordlist.txt"
-
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <map>
 #include <bitset>
+#include <vector>
+#include <algorithm>
 
-
+#define WORDLIST_NAME "wordlist.txt"
+#define ENCODED_EXTENSION ".tok"
 
 using namespace std;
+using WordList_T = std::map<string, uint8_t>;
 
 template<typename T>
 inline T LoadFile(const std::string& filename, const std::string& fileClass = "File");
 
-
-void Encode(fstream file);
-void Decode(fstream file);
+void Encode(ifstream &file, string OutFileName, const WordList_T &wordlist);
+void Decode(ifstream &file, string OutFileName, const WordList_T &wordlist);
+bool isalpha(string str);
 
 int main(int argc, char *argv[])
 {
     if(argc != 3)
     {
-        cout << "Usage: " << argv[0] << " <input> <output>" << endl;
+        cout << "Usage: " << argv[0] << " <input> <output> \n" << endl;
         return 1;
     }
 
 
     //Loads the supplied file and the wordlist
     
-    ifstream fin = LoadFile<ifstream>(argv[1], "input");   
+    ifstream fin = LoadFile<ifstream>(argv[1], "Input");   
     ifstream WordList(WORDLIST_NAME);
 
     if(!WordList || WordList.bad())
     {
-        cout << "Wordlist file not found! \nThe file has to be named wordlist.txt!" << endl;
+        cout << "Wordlist file not found! \nThe file has to be named wordlist.txt! \n" << endl;
         return 1;
     }
     
-    map<string,uint8_t> wordlist;
+    WordList_T wordlist;
     
     int linecount = 0;
     string line;
@@ -51,17 +52,35 @@ int main(int argc, char *argv[])
     
     string OutFileName{argv[2]};
     
-    string::size_type DotPos = OutFileName.find(".", 0);
+    string::size_type OutDotPos = OutFileName.find(".", 0);
     
-    if(DotPos != 0 )
+    if(OutDotPos != string::npos )
     {
-        OutFileName = OutFileName.substr(0,DotPos );
+        OutFileName = OutFileName.substr(0,OutDotPos );
     }
 
-    cout << OutFileName;
+    
 
     string InFileName{argv[1]};
-    string InFileExtension = InFileExtension.substr(InFileName.find("."), InFileName.length());
+
+    string InFileExtension{""};
+
+    string::size_type InDotPos = InFileName.find(".", 0);
+    if(InDotPos != string::npos )
+    {
+        InFileExtension = InFileName.substr(InDotPos );
+    }
+    
+    if(InFileExtension == ".txt")
+    {
+        Encode(fin, OutFileName, wordlist);
+    }
+    else
+    {
+        Decode(fin, OutFileName, wordlist );
+    }
+    
+    
 
 
     fin.close();
@@ -80,17 +99,66 @@ inline T LoadFile(const std::string& filename, const std::string& fileClass)
 
     if (!filestream || filestream.bad())
     {
-        std::cerr << fileClass << " not found!" << std::endl;
+        std::cerr << fileClass << " could not be loaded!" << std::endl;
         std::exit(EXIT_FAILURE);
     }
 
     return filestream;
 }
-void Encode(fstream file)
+void Encode(ifstream &file, string OutFileName, const WordList_T &wordlist)
+{
+    ofstream OutFile = LoadFile<ofstream>(OutFileName + ENCODED_EXTENSION, "Output");
+
+
+    string word{};
+
+    std::vector<std::string> words;
+    
+    while(file >> word)
+    {
+        
+       
+
+        if(isalpha(word))
+        {
+            words.push_back(word);
+        }
+        else
+        {
+            auto SplitPoint = find_if(word.begin(), word.end(), [](char c) { return !isalpha(c); } );
+            
+            words.push_back(word.substr(0, distance(word.begin(), SplitPoint)));
+            words.push_back(word.substr(distance(word.begin(), SplitPoint)));
+        }
+        
+    }
+    for(auto str : words)
+    {
+        cout << str << " " << (unsigned int) wordlist.at(str) << endl;
+        OutFile.write(  (char*) &wordlist.at(str), sizeof(uint8_t));
+    }
+
+
+
+    OutFile.close();
+
+}
+void Decode(ifstream &file, string OutFileName, const WordList_T &wordlist)
 {
 
 }
-void Decode(fstream file)
+bool isalpha(string str)
 {
-
+    for(char cha : str)
+    {
+        if(isalpha(cha))
+        {
+            continue;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
 }
